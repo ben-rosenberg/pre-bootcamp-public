7c14627779ae40855594a07122c35171b5b2ad7d
Ben Rosenberg <brosenberg570@gmail.com>
2022-02-19 19:03:27 -0700

    Initial commit

---
+++ ROSENBERG_BEN_JS_Tinkering.js
@@ -0,0 +1,14 @@
+// Funny enough, something like this happened to me when I was a kid
+
+var childHeight = 51;
+
+function displayWhetherChildIsAbleToRideTheRollerCoaster(inputHeight)
+{
+    if (inputHeight >= 52)
+        console.log("Get on that ride, kiddo!");
+
+    else
+        console.log("Sorry kiddo. Maybe next year.");
+}
+
+displayWhetherChildIsAbleToRideTheRollerCoaster(childHeight);
